bf2075fcff4c209d9174d390ae08ad6e921abe07
spgdaman <gatheru.simon432@gmail.com>
2019-02-19 14:12:25 +0300

    Add content, jQuery functions and definitions

---
+++ js/scripts.js
@@ -0,0 +1,176 @@
+$(document).ready(function() {
+  $("#js").click(function() {
+    $("#show-js").fadeIn(50);
+    console.log("Hey, I faded in");
+    setTimeout(function() {
+      $("#show-js").fadeOut(1000);
+    },2000);
+    console.log("Hey, I faded out");
+  });
+
+  $("#operators").click(function() {
+    $("#show-operators").fadeIn(50);
+    console.log("Hey, I faded in");
+    setTimeout(function() {
+      $("#show-operators").fadeOut(1000);
+    },2000);
+    console.log("Hey, I faded out");
+  });
+
+  $("#variables").click(function() {
+    $("#show-variables").fadeIn(50);
+    console.log("Hey, I faded in");
+    setTimeout(function() {
+      $("#show-variables").fadeOut(1000);
+    },2000);
+    console.log("Hey, I faded out");
+  });
+
+  $("#conventions").click(function() {
+    $("#show-conventions").fadeIn(50);
+    console.log("Hey, I faded in");
+    setTimeout(function() {
+      $("#show-conventions").fadeOut(1000);
+    },2000);
+    console.log("Hey, I faded out");
+  });
+
+  $("#functions").click(function() {
+    $("#show-functions").fadeIn(50);
+    console.log("Hey, I faded in");
+    setTimeout(function() {
+      $("#show-functions").fadeOut(1000);
+    },2000);
+    console.log("Hey, I faded out");
+  });
+
+  $("#string").click(function() {
+    $("#show-string").fadeIn(50);
+    console.log("Hey, I faded in");
+    setTimeout(function() {
+      $("#show-string").fadeOut(1000);
+    },2000);
+    console.log("Hey, I faded out");
+  });
+
+  $("#methods").click(function() {
+    $("#show-methods").fadeIn(50);
+    console.log("Hey, I faded in");
+    setTimeout(function() {
+      $("#show-methods").fadeOut(1000);
+    },2000);
+    console.log("Hey, I faded out");
+  });
+
+  $("#arguments").click(function() {
+    $("#show-arguments").fadeIn(50);
+    console.log("Hey, I faded in");
+    setTimeout(function() {
+      $("#show-arguments").fadeOut(1000);
+    },2000);
+    console.log("Hey, I faded out");
+  });
+
+  $("#parameters").click(function() {
+    $("#show-parameters").fadeIn(50);
+    console.log("Hey, I faded in");
+    setTimeout(function() {
+      $("#show-parameters").fadeOut(1000);
+    },2000);
+    console.log("Hey, I faded out");
+  });
+
+  $("#return").click(function() {
+    $("#show-return").fadeIn(50);
+    console.log("Hey, I faded in");
+    setTimeout(function() {
+      $("#show-return").fadeOut(1000);
+    },2000);
+    console.log("Hey, I faded out");
+  });
+
+  $("#chaining").click(function() {
+    $("#show-chaining").fadeIn(50);
+    console.log("Hey, I faded in");
+    setTimeout(function() {
+      $("#show-chaining").fadeOut(1000);
+    },2000);
+    console.log("Hey, I faded out");
+  });
+
+  $("#booleans").click(function() {
+    $("#show-booleans").fadeIn(50);
+    console.log("Hey, I faded in");
+    setTimeout(function() {
+      $("#show-booleans").fadeOut(1000);
+    },2000);
+    console.log("Hey, I faded out");
+  });
+
+  $("#undefined").click(function() {
+    $("#show-undefined").fadeIn(50);
+    console.log("Hey, I faded in");
+    setTimeout(function() {
+      $("#show-undefined").fadeOut(1000);
+    },2000);
+    console.log("Hey, I faded out");
+  });
+
+  $("#Nan").click(function() {
+    $("#show-NaN").fadeIn(50);
+    console.log("Hey, I faded in");
+    setTimeout(function() {
+      $("#show-NaN").fadeOut(1000);
+    },2000);
+    console.log("Hey, I faded out");
+  });
+
+  $("#escape").click(function() {
+    $("#show-escape").fadeIn(50);
+    console.log("Hey, I faded in");
+    setTimeout(function() {
+      $("#show-escape").fadeOut(1000);
+    },2000);
+    console.log("Hey, I faded out");
+  });
+
+  $("#alert").click(function() {
+    $("#show-alert").fadeIn(50);
+    console.log("Hey, I faded in");
+    setTimeout(function() {
+      $("#show-alert").fadeOut(1000);
+    },2000);
+    console.log("Hey, I faded out");
+  });
+
+  $("#comments").click(function() {
+    $("#show-comments").fadeIn(50);
+    console.log("Hey, I faded in");
+    setTimeout(function() {
+      $("#show-comments").fadeOut(1000);
+    },2000);
+    console.log("Hey, I faded out");
+  });
+
+  $("#jQuery").click(function() {
+    $("#show-jQuery").fadeIn(50);
+    console.log("Hey, I faded in");
+    setTimeout(function() {
+      $("#show-jQuery").fadeOut(1000);
+    },2000);
+    console.log("Hey, I faded out");
+  });
+
+  $("#attributes").click(function() {
+    $("#show-attributes").fadeIn(50);
+    console.log("Hey, I faded in");
+    setTimeout(function() {
+      $("#show-attributes").fadeOut(1000);
+    },2000);
+    console.log("Hey, I faded out");
+  });
+});
+
+
+
+ // setTimeout(fadeaway("#show-js"),50)
